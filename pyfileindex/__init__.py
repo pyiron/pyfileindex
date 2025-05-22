fcbfc427f36d673bd9662f4411df73b11799df84
Jan Janssen <janssen@mpie.de>
2025-05-22 10:06:52 +0200

    Switch from setuptools to hatchling

--- pyfileindex/__init__.py
+++ pyfileindex/__init__.py
@@ -2,5 +2,5 @@ from pyfileindex.pyfileindex import PyFileIndex
 
 from . import _version
 
-__version__ = _version.get_versions()["version"]
+__version__ = _version.__version__
 __all__ = ["PyFileIndex"]
