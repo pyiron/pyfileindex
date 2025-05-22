11460fbaa5ff302fc89eebb80ef474103b085f22
Jan Janssen <janssen@mpie.de>
2025-05-22 11:37:02 +0200

    ignore files on git

--- pyfileindex/__init__.py
+++ pyfileindex/__init__.py
@@ -1,4 +1,5 @@
 from pyfileindex.pyfileindex import PyFileIndex
-from pyfileindex.version import __version__
+from . import _version
 
-__all__ = ["PyFileIndex", "__version__"]
+__all__ = ["PyFileIndex"]
+__version__ = _version.__version__
