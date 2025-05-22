b232d7e12a264221cea9adf07142bd5a3b11e626
pyiron-runner <pyiron@mpie.de>
2025-05-22 08:08:11 +0000

    Format black

--- pyfileindex/_version.py
+++ pyfileindex/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.33.dev28+gb11352d.d20250522'
-__version_tuple__ = version_tuple = (0, 0, 33, 'dev28', 'gb11352d.d20250522')
+__version__ = version = "0.0.33.dev28+gb11352d.d20250522"
+__version_tuple__ = version_tuple = (0, 0, 33, "dev28", "gb11352d.d20250522")
